771d493b11b30c27051e443218d002faa91effc2
sunkatheleen <katheleensantiago@hotmail.com>
2025-07-18 23:56:06 -0300

    novos exercícios do dia 18

---
+++ src/fundamentos/ExerciciosSimples3.java
@@ -0,0 +1,24 @@
+// Área: Calcule a área de um retângulo.
+//
+
+package fundamentos;
+
+import java.util.Scanner;
+
+public class ExercicioSimples3 {
+    public static void main (String[] args) {
+
+        Scanner calculo = new Scanner(System.in);
+
+        System.out.println("Vamos calcular a área de um retângulo? digite qual é o valor da base: ");
+        double base = calculo.nextDouble();
+
+        System.out.println("Agora digite a altura: ");
+        double altura = calculo.nextDouble();
+
+        double area = base * altura;
+
+        System.out.println("A área desse retãngulo é: " + area + "cm²");
+
+    }
+}
